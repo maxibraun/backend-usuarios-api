84290893f4676308d88cecc773e870769329bc98
Maximiliano Braun <mbraun@securitasargentina.com>
2026-04-22 08:30:48 -0300

    Creación de tablas

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from routes.usuarios import usuarios_bp
 
 app = Flask(__name__)
 
+from database import engine
+from models import Base
+Base.metadata.create_all(bind=engine)
+
 # registrar rutas
 app.register_blueprint(usuarios_bp)
 
